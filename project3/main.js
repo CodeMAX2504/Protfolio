7e4cfe3e7fd600d805b67522a7b1ca50d6164047
Mintu Nandi <nandimintu096@gmail.com>
2024-10-15 10:44:39 +0530

    first commit

---
+++ project3/main.js
@@ -0,0 +1,71 @@
+/* ********************toggle icon navebar******************************* */ 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+
+
+// let menuIcon = document.querySelector('#menu-icon');
+// let navbar = document.querySelector('.navbar');
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('bx-x'); // Toggle the 'bx-x' class to change icon to 'X'
+//     navbar.classList.toggle('active'); // Toggle 'active' class to show/hide the navbar
+// };
+
+
+/* ********************scroll section link******************************* */ 
+ let sections = document.querySelectorAll('section');
+ let navLinks = document.querySelectorAll('header nav a');
+ window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+          if(top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                });
+            }
+    });
+/* ********************sticky navbar******************************* */
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* ********************remove toggle icon and  navbar ******************************* */
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+
+};
+
+
+/* ********************scroll reveal******************************* */
+ScrollReveal({ 
+    distance: '80px',
+    duration: 2000,
+    delay:200,
+});
+
+ScrollReveal().reveal('.home-content,heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container,.protfolio-box,.contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/* ********************type js******************************* */
+var typed =new Typed('.multiple-text',{
+    strings:['Frontend Developer','Web Developer','Full Stack Developer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true
+})
